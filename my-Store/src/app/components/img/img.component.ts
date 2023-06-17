fd1f0e9ff006ed961e9a5cefa08a65220a3cdd6c
Kevin Julian Acevedo Salazar <kjacevedos@outsourcing.com.co>
2023-06-17 15:29:59 -0500

    Output Component

--- my-Store/src/app/components/img/img.component.ts
+++ my-Store/src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -6,7 +6,16 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./img.component.scss']
 })
 export class ImgComponent {
-  @Input() img: string = 'Valor Inicial';
+  @Input() img: string = '';
+  @Output() loaded = new EventEmitter<string>();
+  imageDefault='./assets/images/404.png';
 
+  imgError(){
+    this.img = this.imageDefault;
+  }
 
+  imgLoaded(){
+    console.log('log hijo');
+    this.loaded.emit(this.img);
+  }
 }
